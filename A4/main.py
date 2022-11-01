82b0f1e6bcd57686f46f3bdd1606b0cb75c82da7
Saul999 <84159357+Saul999@users.noreply.github.com>
2022-11-01 14:10:15 -0700

    Finished

--- A4/main.py
+++ A4/main.py
@@ -1,1 +1,37 @@
+from weather import *
+
+myfile = "weather.dat"
+
+mychoice = 0
+while(True):
+    print("     *** TUFFY TITAN WEATHER LOGGER MAIN MENU")
+    print()
+    print("1. Set data Filename")
+    print("2. Add Weather Data")
+    print("3. Print Daily Report")
+    print("9. Exit the program")
+
+    choice = input("Enter Data Filename")
+
+    print()
+    if mychoice == 1:
+        myfile = input("Enter Data Filename:")
+        weather = read_data(myfile)
+    elif mychoice  == 2:
+        dt = int(input("Enter date: "))
+        tm = input("enter time: ")
+        t = int(input("Enter humidity: "))
+        h = int(input("Enter the Rainfall: "))
+        r = float(input("Enter rainfall: "))
+        weather[dt+tm] = {'t':t,'h':h,'r':r}
+        write_data(weather, myfile)
+    elif mychoice == 3:
+        d = input("Enter Date: ")
+        display = report_daily(data = weather, date = d)
+        print(display)
+
+    elif mychoice == 4:
+        display = report_historical(data = weather)
+        print(display)
+
 
